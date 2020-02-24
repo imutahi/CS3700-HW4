b5ff0645995619d959b0cb54716f4f5c138c6050
Raizex <jwhuffman@gmail.com>
2020-02-24 14:54:15 -0700

    Wrote initialize connection function for server.

--- TCPMultiServerThread.java
+++ TCPMultiServerThread.java
@@ -9,6 +9,7 @@ import java.io.*;
 
 public class TCPMultiServerThread extends Thread {
     private Socket clientTCPSocket = null;
+    private int state = 0;
 
     public TCPMultiServerThread(Socket socket) {
 		super("TCPMultiServerThread");
@@ -18,28 +19,38 @@ public class TCPMultiServerThread extends Thread {
     public void run() {
 
 		try {
-	 	   PrintWriter cSocketOut = new PrintWriter(clientTCPSocket.getOutputStream(), true);
-	  		BufferedReader cSocketIn = new BufferedReader(
-				    new InputStreamReader(
-				    clientTCPSocket.getInputStream()));
-
-	      String fromClient, toClient;
+	 	    PrintWriter cSocketOut = new PrintWriter(clientTCPSocket.getOutputStream(), true);
+            BufferedReader cSocketIn = new BufferedReader(
+                new InputStreamReader(
+                    clientTCPSocket.getInputStream()));
 			  
-	 	   while ((fromClient = cSocketIn.readLine()) != null) {
+            while (this.state != -1) {
+                if (this.state == 0) {
+                    this.state = initializeConnection(cSocketOut);
+                } else {
+                    this.state = -1;
+                }
 				
-				toClient = fromClient.toUpperCase();
-				cSocketOut.println(toClient);
+				// toClient = fromClient.toUpperCase();
+				// cSocketOut.println(toClient);
 				
-				if (fromClient.equals("Bye"))
-				    break;
-	 	   }
+				// if (fromClient.equals("Bye"))
+				//    break;
+            }
 			
-		   cSocketOut.close();
-		   cSocketIn.close();
-		   clientTCPSocket.close();
+            cSocketOut.close();
+            cSocketIn.close();
+            clientTCPSocket.close();
 
 		} catch (IOException e) {
 		    e.printStackTrace();
 		}
+    }
+
+    private int initializeConnection(PrintWriter cSocketOut) {
+        String hostname = java.net.InetAddress.getHostName();
+        String toClient = "220 " + hostname;
+        cSocketOut.println(toClient);
+        return 1;
     }
 }
