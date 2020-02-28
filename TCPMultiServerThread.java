d3bd919315ab745c99b9f2fc4708fe58d4175937
Raizex <jwhuffman@gmail.com>
2020-02-27 18:19:34 -0700

    Added logic for QUIT and invalid response.

--- TCPMultiServerThread.java
+++ TCPMultiServerThread.java
@@ -33,6 +33,11 @@ public class TCPMultiServerThread extends Thread {
             while (this.state != -1) {
                 if (this.state == 0) {
                     this.state = initializeConnection(cSocketOut);
+                } else if(this.state == 1) {
+                    String clientMessage = cSocketIn.readLine();
+                    ParseResponse response = parseRequest(this.state, clientMessage);
+                    this.state = response.newState;
+                    if (response.response != null) {cSocketOut.print(response.response);}
                 } else {
                     this.state = -1;
                 }
@@ -55,13 +60,22 @@ public class TCPMultiServerThread extends Thread {
 
     ParseResponse parseRequest(int currentState, String clientMessage) {
          ParseResponse response = new ParseResponse();
+
     	 if(currentState == 1) { 
     		 String[] words = clientMessage.split(" ", 2);
+
     		 if(words[0].equals("HELO")) {
-    			 response.response = "250 " + this.hostAddress.toString() + " Hello " + this.clientAddress.toString() + "\r\n";
     			 response.newState = 2;
-    		 }
+    			 response.response = "250 " + this.hostAddress.getHostAddress() + " Hello " + this.clientAddress.getHostAddress() + "\r\n";
+    		 } else if (words[0].equals("QUIT")) {
+                 response.newState = -1;
+                 response.response = "221 " + this.hostAddress.getHostAddress() + " closing connection\r\n";
+             } else {
+                 response.newState = 1;
+                 response.response = "503 5.5.2 Send hello first\r\n";
+             }
     	 }
+
          return response;
     }
 
